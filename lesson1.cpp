e1d6bed6f0d3a2a77f07e1092f063742a249070f
hunterhug <gdccmcm14@live.com>
2017-09-05 15:25:06 +0800

    C++

---
+++ lesson1.cpp
@@ -0,0 +1,48 @@
+/*
+ *  C++ 第一课!!
+ *  g++ lesson1.cpp -o l1
+ * */
+#include <iostream>
+
+// 命名空间
+using namespace std;
+
+// 类型定义
+typedef int feet;
+
+// 枚举
+enum color {
+    red, green = 5, blue
+};
+enum color1 {
+    red1, green1 = 5, blue1
+} ccc;
+
+
+// 函数
+int func() {
+    cout << "Size of char : " << sizeof(char) << endl;
+    cout << "Size of int : " << sizeof(int) << endl;
+    cout << "Size of short int : " << sizeof(short int) << endl;
+    cout << "Size of long int : " << sizeof(long int) << endl;
+    cout << "Size of float : " << sizeof(float) << endl;
+    cout << "Size of double : " << sizeof(double) << endl;
+    cout << "Size of wchar_t : " << sizeof(wchar_t) << endl;
+}
+
+// 主入口
+int main() {
+    ccc = green1;
+    cout << ccc << endl;
+
+    color c = color(6);
+    color cc = red;
+    cout << c << endl;
+    cout << cc << endl;
+
+    // 打印
+    func();
+
+    //    cout << "Hel\?\?-  ~ /?lo, wor??ld!\n\n\n\n\n\n\n\nddd" << endl;
+    return 0;
+}
